6714f650781615db214f56fbfbaa937e726d27a8
fishyclicker1234 <sevierjames8@gmail.com>
2025-10-01 17:48:38 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-
-const fish = document.getElementById("fish");
-const scoreDisplay = document.getElementById("score");
-
-fish.addEventListener("click", () => {
-  score++;
-  scoreDisplay.textContent = "Score: " + score;
-});
